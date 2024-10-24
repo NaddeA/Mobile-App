df771cca4e02c177c96968abb0c30aa77d2ec2a8
Mustafa.Qassmieh01 <mustafqasmieh@gmail.com>
2024-10-24 12:06:19 +0200

    starting off with basic layout for bluetooth

--- app/src/main/java/com/example/mobileapp_project/Master/Bluetooth.kt
+++ app/src/main/java/com/example/mobileapp_project/Master/Bluetooth.kt
@@ -1,17 +1,68 @@
 package com.example.mobileapp_project.Master
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.pm.PackageManager
+import android.util.Log
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 
 
-class BluetoothHelper(private val context: Context) {
-    val bluetoothAvailable = context.packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)
-    val bluetoothLEAvailable = context.packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
+class Bluetooth(private val context: Context) {
+
+    // Package Manager creating an instance to use
+    val packageManager= context.packageManager
+
+    //Creating a Bluetooth manager and from it an adapter
+    val bluetoothManager : BluetoothManager = context.getSystemService(BluetoothManager::class.java)
+    val bluetoothAdapter : BluetoothAdapter? = bluetoothManager.getAdapter()
+
+    // Checking if normal bluetooth is available ( Mostly for premative sensors and older phones), then Low battery bluetooth
+    // Checking if it is enabled
+    val bluetoothLEAvailable : Boolean
+            get() = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
+    val bluetoothAvailable: Boolean
+        get() =  packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)
+    val bluetoothEnabled : Boolean
+        get() = bluetoothAdapter?.isEnabled() == true
+
+    val bluetooth = Bluetooth(context)
+
+    // Method to check if Bluetooth permission is granted
+    private fun isBluetoothPermissionGranted(): Boolean {
+        val bluetoothPermission = ContextCompat.checkSelfPermission(
+            context,
+            android.Manifest.permission.BLUETOOTH
+        )
+        val bluetoothAdminPermission = ContextCompat.checkSelfPermission(
+            context,
+            android.Manifest.permission.BLUETOOTH_ADMIN
+        )
+
+        return bluetoothPermission == PackageManager.PERMISSION_GRANTED &&
+                bluetoothAdminPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    @SuppressLint("MissingPermission") // suppressing permission because I already checked for in the //isBluetoothPermissionGranted\\ Method
+    fun connectToDevice(deviceName: String) {
+        if (!bluetoothAvailable || !bluetoothLEAvailable ||!bluetoothEnabled ) {
+            Log.e("BluetoothHelper", "Bluetooth not supported or enabled.")
+            return
+        }
+        if (isBluetoothPermissionGranted()) {
+            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        }
+    }
+
 }
+
+
+
 
 
 
