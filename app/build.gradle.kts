9fa37ab39cbe0b72fc8fc9ad042b0179a83f6804
Mustafa.Qassmieh01 <mustafqasmieh@gmail.com>
2024-11-07 16:04:00 +0100

    N/A

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.mobileapp_project"
-        minSdk = 21
+        minSdk = 25
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/mobileapp_project/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp_project/MainActivity.kt
@@ -1,23 +1,68 @@
 package com.example.mobileapp_project
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val bluetoothManager by lazy {
+        applicationContext.getSystemService(BluetoothManager::class.java)
+    }
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // bluetooth elements
+        val enableBluetoothLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { /* Not needed */ }
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { perms ->
+            val canEnableBluetooth = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                perms[Manifest.permission.BLUETOOTH_CONNECT] == true
+            } else true
 
+            if(canEnableBluetooth && !isBluetoothEnabled) {
+                enableBluetoothLauncher.launch(
+                    Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                )
+            }
+        }
         val bluetoothSettingsButton = findViewById<Button>(R.id.bluetoothSettingsButton)
         bluetoothSettingsButton.setOnClickListener {
             val intent = Intent(this, BluetoothSettingsActivity::class.java)
@@ -71,4 +116,63 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
+// the commented UI arguments is for refrance on how the bluetooth is supposed to work
+//setContent {
+//    BluetoothChatTheme {
+//        val viewModel = hiltViewModel<BluetoothViewModel>()
+//        val state by viewModel.state.collectAsState()
+//
+//        LaunchedEffect(key1 = state.errorMessage) {
+//            state.errorMessage?.let { message ->
+//                Toast.makeText(
+//                    applicationContext,
+//                    message,
+//                    Toast.LENGTH_LONG
+//                ).show()
+//            }
+//        }
+//
+//        LaunchedEffect(key1 = state.isConnected) {
+//            if(state.isConnected) {
+//                Toast.makeText(
+//                    applicationContext,
+//                    "You're connected!",
+//                    Toast.LENGTH_LONG
+//                ).show()
+//            }
+//        }
+//
+//        Surface(
+//            color = MaterialTheme.colors.background
+//        ) {
+//            when {
+//                state.isConnecting -> {
+//                    Column(
+//                        modifier = Modifier.fillMaxSize(),
+//                        horizontalAlignment = Alignment.CenterHorizontally,
+//                        verticalArrangement = Arrangement.Center
+//                    ) {
+//                        CircularProgressIndicator()
+//                        Text(text = "Connecting...")
+//                    }
+//                }
+//                state.isConnected -> {
+//                    ChatScreen(
+//                        state = state,
+//                        onDisconnect = viewModel::disconnectFromDevice,
+//                        onSendMessage = viewModel::sendMessage
+//                    )
+//                }
+//                else -> {
+//                    DeviceScreen(
+//                        state = state,
+//                        onStartScan = viewModel::startScan,
+//                        onStopScan = viewModel::stopScan,
+//                        onDeviceClick = viewModel::connectToDevice,
+//                        onStartServer = viewModel::waitForIncomingConnections
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
